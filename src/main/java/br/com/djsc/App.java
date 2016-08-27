2565a851759b130a76c199288ea33f0b0b73cec4
Danilo Correa <danilo@condomni.com.br>
2016-08-27 19:33:23 -0300

    Codificação do da classe Main

--- src/main/java/br/com/djsc/App.java
+++ src/main/java/br/com/djsc/App.java
@@ -1,13 +1,31 @@
 package br.com.djsc;
 
+import java.util.Scanner;
+
+import br.com.djsc.engine.CharSearchEngine;
+import br.com.djsc.stream.Stream;
+import br.com.djsc.stream.StreamImpl;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = null;
+
+		do {
+			System.out.print("Informe o stream de entrada: ");
+			str = scanner.nextLine();
+
+			try {
+				Stream stream = new StreamImpl(str);
+				char firstChar = CharSearchEngine.firstChar(stream);
+				System.out.println(String.format("Character = %c", firstChar));
+			} catch (Exception e) {
+				System.out.println(String.format("Ops: %s", e.getMessage()));
+			}
+		} while (str == "");
+	}
 }
